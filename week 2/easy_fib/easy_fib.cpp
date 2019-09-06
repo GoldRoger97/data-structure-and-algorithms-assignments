b38d36602a97d24c06c59cb77ce2b98033577e84
GoldRoger97 <53105187+GoldRoger97@users.noreply.github.com>
2019-09-06 17:37:06 +0200

    Create easy_fib.cpp

---
+++ week 2/easy_fib/easy_fib.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+
+int main() {
+    int n = 0;//O(1)
+    cin>>n;//O(1)
+    int fibbo[n + 1];//O(n + 1)
+    fibbo[0] = 0;//O(1)
+    fibbo[1] = 1;//O(1)
+    
+    for(int i = 2; i <= n; i++)
+    fibbo[i] = fibbo[i - 1] + fibbo[i - 2];
+    cout<<fibbo[n];
+    return 0;
+}
